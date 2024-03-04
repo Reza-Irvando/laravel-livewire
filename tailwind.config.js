2541e1b534701fd8a1590d2be5a93911d7d36d33
Pando <rezairvando@mail.ugm.ac.id>
2024-03-04 14:11:12 +0700

    tes

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+import typhography from '@tailwindcss/typography'
+
+export default {
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+    variants: {
+        extend: {
+            backgroundColor: ['active'],
+        }
+    },
+    content: [
+        './app/**/*.php',
+        './resources/**/*.html',
+        './resources/**/*.js',
+        './resources/**/*.jsx',
+        './resources/**/*.ts',
+        './resources/**/*.tsx',
+        './resources/**/*.php',
+        './resources/**/*.vue',
+        './resources/**/*.twig',
+    ],
+    plugins: [forms, typhography],
+}
